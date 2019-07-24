533dc34f63acf9b86a8e343b07da36d6fb55f59d
Averroess <neil-1994@outlook.com>
2019-07-24 09:31:29 +0800

    增加匿名内部类

--- shengsiyuan/JAVA_SE/src/InnerClass/AnonymousInnerClassTest.java
+++ shengsiyuan/JAVA_SE/src/InnerClass/AnonymousInnerClassTest.java
@@ -1,5 +1,7 @@
 package InnerClass;
 
+import java.util.Date;
+
 /**
  * @program: JavaSePractice
  * @description:
@@ -7,5 +9,21 @@ package InnerClass;
  * @create: 2019-07-23 13:35
  **/
 public class AnonymousInnerClassTest {
+    @SuppressWarnings("deprecation")
+    public String get(Date date){
+        return date.toLocaleString();
+    }
 
+    public static void main(String[] args) {
+        AnonymousInnerClassTest anonymousInnerClassTest = new AnonymousInnerClassTest();
+        String str = anonymousInnerClassTest.get(new Date());
+        System.out.println(str);
+
+        String str1 = anonymousInnerClassTest.get(new Date() {
+            public String toLocaleString(){
+                return "hello world";
+            }
+        });
+        System.out.println(str1);
+    }
 }
